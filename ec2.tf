0e74f5c78c0ccc3e957dfd019dbb35efb5d3f1cd
Tony Ike Ozouli <flawlez18@yahoo.co.uk>
2023-08-13 15:47:53 -0500

    modify ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,18 @@
-provider "aws" {
-  region = us-east-1
-}
+# provider "aws" {
+#   region = us-east-1
+# }
 
-resource "aws_instance" "demo" {
-  ami           = data.aws_ami.amzn-linux-2023-ami.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.example.id
+# resource "aws_instance" "demo" {
+#   ami           = data.aws_ami.amzn-linux-2023-ami.id
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.example.id
 
-  cpu_options {
-    core_count       = 2
-    threads_per_core = 2
-  }
+#   cpu_options {
+#     core_count       = 2
+#     threads_per_core = 2
+#   }
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
